e8bddfcca85332867e9f4fbaf48f2f7c7d73a26d
Yevhen <Evgen743@gamil.com>
2022-03-15 23:56:24 +0200

    add interface UniqueLogin

---
+++ src/main/java/ua/gov/mkip/saveheritage/validator/UniqueLogin.java
@@ -0,0 +1,18 @@
+package ua.gov.mkip.saveheritage.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import ua.gov.mkip.saveheritage.validator.UniqueLoginValidator;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueLoginValidator.class)
+public @interface UniqueLogin {
+    String message() default "{message.notUniqueLogin}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
